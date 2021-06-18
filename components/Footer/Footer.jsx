56e70a44f9a874a95bb4dd2d58341f5fc4b9b404
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2021-06-18 14:51:04 +0300

    features: footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -10,41 +10,52 @@ import { routes } from "../../constants/routes";
 
 // assets
 import styles from './Footer.module.scss'
-import logo from '../../public/icons/logo.svg'
 import twitter from '../../public/icons/twitter.svg'
 import instagram from '../../public/icons/instagram.svg'
 import facebook from '../../public/icons/facebook.svg'
+import linkenin from '../../public/icons/linkenin.svg'
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerTop}>
-        <Image src={logo} alt="Eduserve Logo" />
-        <div className={styles.footerTopInner}>
+      <div className='container'>
+        <div className={styles.footerInner}>
+          <div className={styles.language}>
+            <select>
+              <option value='1'>English</option>
+              <option value='2'>Русский</option>
+            </select>
+          </div>
+          <ul className={styles.links}>
+            <li>CopyRight 2021 ©Edu Serve</li>
+            <li><Link href={routes.privacy}><a>Privacy Statement</a></Link></li>
+            <li><Link href={routes.terms}><a>Terms of Service</a></Link></li>
+            <li><Link href={routes.contact}><a>Contact Us</a></Link></li>
+          </ul>
           <ul className={styles.social}>
+            <li>
+              <a href="#" target="_blank" rel="noreferrer">
+                <Image src={facebook} alt="facebook" />
+              </a>
+            </li>
+            <li>
+              <a href="#" target="_blank" rel="noreferrer">
+                <Image src={instagram} alt="instagram" />
+              </a>
+            </li>
+            <li>
+              <a href='#' target="_blank" rel="noreferrer">
+                <Image src={linkenin} alt="linkenin" />
+              </a>
+            </li>
             <li>
               <a href='#' target="_blank" rel="noreferrer">
                 <Image src={twitter} alt="twitter" />
               </a>
             </li>
-            <li className={styles.social}>
-              <a href="#" target="_blank" rel="noreferrer">
-                <Image src={instagram} alt="instagram" />
-              </a>
-            </li>
-            <li className={styles.social}>
-              <a href="#" target="_blank" rel="noreferrer">
-                <Image src={facebook} alt="facebook" />
-              </a>
-            </li>
           </ul>
         </div>
       </div>
-      <ul className={styles.footerBottom}>
-        <li><Link href={routes.privacy}><a>Privacy Statement</a></Link></li>
-        <li><Link href={routes.terms}><a>Terms of Service</a></Link></li>
-        <li><Link href={routes.contact}><a>Contact Us</a></Link></li>
-      </ul>
     </footer>
   )
 };
--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -24,7 +24,7 @@ export const Layout = ({children}) => {
       </Head>
       <Header />
       <main>{children}</main>
-      {/*<Footer />*/}
+      <Footer />
     </>
   )
 };
